41bf01cf8a001ee8d0acde9ad12cd76b27cb7b38
Filigs <40748182+Filigs@users.noreply.github.com>
2022-11-19 16:40:47 +0000

    working

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+import("tailwindcss").Config;
+
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**.{js,ts,jsx,tsx}",
+    "./components/**.{js,ts,jsx,tsx}",
+    "./components/**/**.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        light: "#ffffff", // White
+        dark: "#0f172a", // slate-900
+      },
+    },
+    plugins: [require("flowbite/plugin")],
+  },
+};
