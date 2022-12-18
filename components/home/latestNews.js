ea69c2e819013604d7d75db1f1101119e7694f84
Filigs <40748182+Filigs@users.noreply.github.com>
2022-12-18 22:49:31 +0000

    index components made responsive
    new "Latest News" component which will render 3 columns of news only, if there are 4 it will only render the first 3 until there are fully 3 available.

--- components/home/latestNews.js
+++ components/home/latestNews.js
@@ -2,35 +2,31 @@ import Image from "next/image";
 import sampleNew from "/public/sampleNew.png";
 export default function LatestNews() {
   const iteration = [1, 2, 3];
-  const listItems = iteration.map(() => {
+  const listItems = iteration.map((item) => {
     return (
-      <div className="grid grid-cols-2 grid-flow-row-dense justify-center justify-items-center bg-bgContainer dark:bg-bgContainerDark rounded-lg py-2 px-4">
-        <div className="block col-span-2">
+      <div
+        className="grid grid-cols-2 bg-bgContainer dark:bg-bgContainerDark rounded-lg py-1.5 lg:py-3 px-3 lg:px-6 mx-auto"
+        key={item}
+      >
+        <div className="block col-span-2 py-2 mx-auto ">
           {/* banner */}
-          <Image
-            src={sampleNew}
-            alt="sample banner"
-            width={300}
-            height={300}
-            id="newsBanner"
-          />
+          <Image src={sampleNew} alt="sample banner" width={300} height={300} />
         </div>
-        <div className="grid grid-flow-row-dense col-span-2 text-start ">
-          <div>
+        <div className="block col-span-2 justify-items-start text-justify py-2">
+          <div className="text-lg lg:text-xl font-bold">
             {/* Title */}
             {"Here be the shiba"}
           </div>
-          <div>
+          <div className="font-light">
             {/* Category */} {"Proof of Work"}
           </div>
         </div>
-        <div className="inline">
-          <div className="justify-start">
-            {/* Author */} {"By Ben Dover"}
-          </div>
-          <div className="justify-end">
-            {/* X days ago */} {"1 day ago"}
-          </div>
+
+        <div className="block text-start font-light py-2">
+          {/* Author */} {"By Ben Dover"}
+        </div>
+        <div className="block text-end py-2 font-light text-slate-500 dark:text-slate-500">
+          {/* X days ago */} {"1 day ago"}
         </div>
       </div>
     );
