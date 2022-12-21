debf38d7863e843e57b641f3f75217842d1f1939
Filigs <40748182+Filigs@users.noreply.github.com>
2022-12-21 21:09:46 +0000

    burger not working in dark mode
    saving point
    working on database for backend uploading

--- components/nav/Menu.js
+++ components/nav/Menu.js
@@ -1,16 +1,18 @@
 import Link from "next/link";
 import { links } from "./navItems";
 import React, { useState } from "react";
-import { HiMenu as Burger, HiOutlineX as CloseBurger } from "react-icons/hi";
+import { HiMenu as Burger } from "react-icons/hi";
 import Drawer from "react-modern-drawer";
-import "react-modern-drawer/dist/index.css";
+// import "react-modern-drawer/dist/index.css";
 
 export default function Menu() {
   // link list for drawer
   const itemList = links.map((link) => (
-    <li key={link.title} className="drawerText">
-      <Link href="/">{link.title}</Link>
-    </li>
+    <Link href={"/" + link.title}>
+      <li key={link.title} className="drawerText">
+        {link.title}
+      </li>
+    </Link>
   ));
   // toggle
   // Menu hook
