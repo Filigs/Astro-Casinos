52df405c9d6a076bc9273e59b6e4c66ab8bf6d55
Filigs <40748182+Filigs@users.noreply.github.com>
2023-01-05 22:32:55 +0000

    update

---
+++ components/Users/news/form/submit.js
@@ -0,0 +1,7 @@
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import React, { useEffect, useState } from "react";
+
+export default function SubmitButton() {
+  const supabase = useSupabaseClient();
+  return;
+}
